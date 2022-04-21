887fa4ee9ed46e7ef4eeb9f1078fa1bb3d7646d4
Andrew Schineller <schineaj23@pwcs-edu.org>
2022-04-20 21:26:17 -0400

    typo again 🥶🍦

--- 4_20/question_two.m
+++ 4_20/question_two.m
@@ -1,8 +1,7 @@
 % Andrew Schineller
 % Question Two
 
-%height = str2num(input("Enter heights: ", "s"));
-height = [1 8 6 2 5 4 8 8 3 7];
+height = str2num(input("Enter heights: ", "s"));
 
 % extremely unoptimized way of doing this O(n^2) time
 % it do work tho
@@ -13,7 +12,6 @@ for i=1:length(height)
         if(i<j)
             area = min(height(i),height(j))*((j-i)-1);
             if (area > max_area)
-                min(height(i), height(j))
                 max_area = area;
                 max_elements = [i,j];
             end
